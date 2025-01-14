288e67acdbdc9470c14490a0cdb0391f20c319ae
leon <1778064763@qq.com>
2025-01-14 13:15:45 +0800

    test sliced and no sliced

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ std::tuple<uint8_t, uint8_t, uint8_t> random_color(int id)
     return hsv2bgr(h_plane, s_plane, 1);
 }
 
-void testInfer()
+void slicedInfer()
 {
    static const char *cocolabels[] = {"person",        "bicycle",      "car",
                                    "motorcycle",    "airplane",     "bus",
@@ -97,14 +97,72 @@ void testInfer()
         // cv::putText(image, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 2, 16);
         
     }
-    printf("Save result to Yolo-result.jpg, %d objects\n", (int)objs.size());
-    cv::imwrite("Yolo-result.jpg", image);
+    printf("Save result to result.jpg, %d objects\n", (int)objs.size());
+    cv::imwrite("result/sliced.jpg", image);
+
+}
+
+void noSlicedInfer()
+{
+   static const char *cocolabels[] = {"person",        "bicycle",      "car",
+                                   "motorcycle",    "airplane",     "bus",
+                                   "train",         "truck",        "boat",
+                                   "traffic light", "fire hydrant", "stop sign",
+                                   "parking meter", "bench",        "bird",
+                                   "cat",           "dog",          "horse",
+                                   "sheep",         "cow",          "elephant",
+                                   "bear",          "zebra",        "giraffe",
+                                   "backpack",      "umbrella",     "handbag",
+                                   "tie",           "suitcase",     "frisbee",
+                                   "skis",          "snowboard",    "sports ball",
+                                   "kite",          "baseball bat", "baseball glove",
+                                   "skateboard",    "surfboard",    "tennis racket",
+                                   "bottle",        "wine glass",   "cup",
+                                   "fork",          "knife",        "spoon",
+                                   "bowl",          "banana",       "apple",
+                                   "sandwich",      "orange",       "broccoli",
+                                   "carrot",        "hot dog",      "pizza",
+                                   "donut",         "cake",         "chair",
+                                   "couch",         "potted plant", "bed",
+                                   "dining table",  "toilet",       "tv",
+                                   "laptop",        "mouse",        "remote",
+                                   "keyboard",      "cell phone",   "microwave",
+                                   "oven",          "toaster",      "sink",
+                                   "refrigerator",  "book",         "clock",
+                                   "vase",          "scissors",     "teddy bear",
+                                   "hair drier",    "toothbrush"};
+
+    cv::Mat image = cv::imread("inference/persons.jpg");
+    // cv::Mat image = cv::imread("6.jpg");
+    auto yolo = yolov11::load("yolov8n.transd.engine");
+    if (yolo == nullptr) return;
+    auto objs = yolo->forward(tensor::cvimg(image), image.cols, image.rows, 0.0f, 0.0f);
+    printf("objs size : %d\n", objs.size());
+    for (auto &obj : objs) 
+    {
+        // obj.dump();
+        uint8_t b, g, r;
+        std::tie(b, g, r) = random_color(obj.class_label);
+        cv::rectangle(image, cv::Point(obj.left, obj.top), cv::Point(obj.right, obj.bottom),
+                    cv::Scalar(b, g, r), 5);
+                    
+        auto name = cocolabels[obj.class_label];
+        auto caption = cv::format("%s %.2f", name, obj.confidence);
+        int width = cv::getTextSize(caption, 0, 1, 2, nullptr).width + 10;
+        // cv::rectangle(image, cv::Point(obj.left - 3, obj.top - 33),
+        //             cv::Point(obj.left + width, obj.top), cv::Scalar(b, g, r), -1);
+        // cv::putText(image, caption, cv::Point(obj.left, obj.top - 5), 0, 1, cv::Scalar::all(0), 2, 16);
+        
+    }
+    printf("Save result to result.jpg, %d objects\n", (int)objs.size());
+    cv::imwrite("result/no_sliced.jpg", image);
 
 }
 
 
 int main()
 {
-    testInfer();
+    slicedInfer();
+    noSlicedInfer();
     return 0;
 }
